5b8f9eba2b3e60db70b7dcb78339a7b18ad5d5be
anjalikadyan <anjalikadyan607@gmail.com>
2025-02-13 21:16:16 +0530

    add more example

---
+++ Delete a node in DLL.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+        new_node.prev = temp
+
+    def delete_node(self, node):
+        if not self.head or not node:
+            return
+        if self.head == node:
+            self.head = node.next
+        if node.next:
+            node.next.prev = node.prev
+        if node.prev:
+            node.prev.next = node.next
+        node = None
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" <-> ")
+            temp = temp.next
+        print("None")
+
+dll = DoublyLinkedList()
+dll.append(1)
+dll.append(2)
+dll.append(3)
+dll.append(4)
+
+dll.delete_node(dll.head.next)  
+dll.display()
