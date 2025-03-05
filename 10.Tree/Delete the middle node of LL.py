682dd774669c5aac93ed291f27d36bd244079f96
anjalikadyan <anjalikadyan607@gmail.com>
2025-03-05 20:44:54 +0530

    add example

---
+++ 10.Tree/Delete the middle node of LL.py
@@ -0,0 +1,41 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def deleteMiddle(head: ListNode) -> ListNode:
+    if not head or not head.next:
+        return None  
+    
+    slow, fast = head, head
+    prev = None
+    
+    while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+    
+    prev.next = slow.next  
+    return head
+
+def list_to_linked_list(arr):
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    current = head
+    for val in arr[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
+
+def linked_list_to_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+input_list = [1, 3, 4, 7, 1, 2, 6]
+head = list_to_linked_list(input_list)
+new_head = deleteMiddle(head)
+print(linked_list_to_list(new_head)) 
