abefa215dc970ae338e3b310c6696fe6e844b203
anjalikadyan <anjalikadyan607@gmail.com>
2025-03-09 19:08:57 +0530

    modified md

---
+++ 10.Tree/Introduction to Trees.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number: "))
+
+def countNodes(i):
+        sum=1
+        j=0
+        while (j<(i-1)):
+            sum*=2
+            j+=1
+        return sum
+print(countNodes(n))
