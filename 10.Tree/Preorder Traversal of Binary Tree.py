d33035793360f8c14561ec46f822374821f4d39a
anjalikadyan <anjalikadyan607@gmail.com>
2025-03-15 20:33:45 +0530

    add example

---
+++ 10.Tree/Preorder Traversal of Binary Tree.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def createTree(self, root, l):
+        if not l:
+            return None
+        
+        root = TreeNode(l[0])
+        queue = deque([root])
+        i = 1
+        
+        while i < len(l):
+            current = queue.popleft()
+            
+            if i < len(l) and l[i] is not None:
+                current.left = TreeNode(l[i])
+                queue.append(current.left)
+            i += 1
+            
+            if i < len(l) and l[i] is not None:
+                current.right = TreeNode(l[i])
+                queue.append(current.right)
+            i += 1
+        
+        return root
+
+
+def preorderTraversal(root):
+    if not root:
+        return []
+    return [root.val] + preorderTraversal(root.left) + preorderTraversal(root.right)
+
+nodes = [1, 2, 3, 4, 5, 6, 7] 
+solution = Solution()
+root = solution.createTree(None, nodes)
+print("Preorder Traversal:", preorderTraversal(root))
