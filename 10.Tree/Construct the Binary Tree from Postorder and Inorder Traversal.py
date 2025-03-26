32c5686ddd4dc333e20844be088cfd0a0dec2e6d
anjalikadyan <anjalikadyan607@gmail.com>
2025-03-26 21:54:57 +0530

    add more question

---
+++ 10.Tree/Construct the Binary Tree from Postorder and Inorder Traversal.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def buildTree(inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+    if not inorder or not postorder:
+        return None
+    
+    root_val = postorder.pop()  
+    root = TreeNode(root_val)
+    
+    inorder_index = inorder.index(root_val)  
+    
+    root.right = buildTree(inorder[inorder_index + 1 :], postorder)
+    root.left = buildTree(inorder[:inorder_index], postorder)
+    
+    return root
+
+def print_preorder(root: Optional[TreeNode]):
+    if not root:
+        return
+    print(root.val, end=" ")
+    print_preorder(root.left)
+    print_preorder(root.right)
+
+inorder = [9, 3, 15, 20, 7]
+postorder = [9, 15, 7, 20, 3]
+root = buildTree(inorder, postorder)
+print_preorder(root)
