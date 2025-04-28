dd33411e31f7131334f1181e8eca997460586746
anjalikadyan <anjalikadyan607@gmail.com>
2025-04-28 22:52:02 +0530

    add more

---
+++ 8.String/Z-Function.py
@@ -0,0 +1,12 @@
+def strStr(haystack: str, needle: str) -> int:
+    if not needle:
+        return 0  
+
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i:i+len(needle)] == needle:
+            return i
+    return -1
+
+haystack = "hello"
+needle = "ll"
+print(strStr(haystack, needle)) 
